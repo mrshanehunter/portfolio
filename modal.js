710b07b75c7c7e0fd72e30e60d458f5d1f5767a2
Shane Hunter <shane_hunter@bigpond.com>
2020-10-05 09:39:10 +1100

    include js scripts

---
+++ modal.js
@@ -0,0 +1,4 @@
+// create an array of objects in another file - a record for each project to populate the-modal 
+
+// eventlistener to pick up id of button being clicked triggers the build and inputs data from the array
+
---
+++ project.js
@@ -0,0 +1,23 @@
+// create the buttons on the projects page using a array.forEach(element => {
+    
+// });
+
+// store the data in an array of objects in this file & a call it on page load  
+
+const projectData = [
+    {id:0, name:'Belvoir', logoImage: './assets/assetsLogo/BFFLogo.png',},
+    {id:1, name:'Thorburn', logoImage: './assets/assetsLogo/ThorburnLogo.png',},
+    {id:2, name:'ATT', logoImage: './assets/assetsLogo/ATTLogo.png',},
+    {id:3, name:'Infrusion', logoImage: './assets/assetsLogo/LogoInfrusion.png',},
+    {id:4, name:'RBY', logoImage: './assets/assetsLogo/RBYLogo.png',},
+    {id:5, name:'tshirts', logoImage: './assets/assetsLogo/tshirtsonlineLogo.png',},
+    {id:6, name:'SAS', logoImage: './assets/assetsLogo/LogoSAS.png',},
+    {id:7, name:'LadyBoss', logoImage: './assets/assetsLogo/LogoLadyBoss.png',}
+];
+
+function createProjectCards () {
+
+projectData.forEach(element => {
+    
+});
+}
